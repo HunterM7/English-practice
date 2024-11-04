04b22b32fd8f717fef35ad27db3a0ba8299f60a1
Anton Meshchaninov <tonymsnv@gmail.com>
2024-11-04 20:28:52 +0300

    Added some styles to the root route.

--- src/router/routes/root.tsx
+++ src/router/routes/root.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/header/header";
+import './root.scss';
 
 export default function Root() {
   return (
-    <>
+    <div className="root">
       <Header/>
-      <div id="detail">
+      <main className="main">
         <Outlet />
-      </div>
-    </>
+      </main>
+    </div>
   );
 }
